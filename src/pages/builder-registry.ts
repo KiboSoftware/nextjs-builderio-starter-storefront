130c51f72ec383c7b1c30e79a3f5614b55ba7ae7
Prathmesh Panchwagh <Prathmesh.p@ignitiv.com>
2024-09-30 12:10:30 +0530

    WEB-905 Component name changed.

--- src/pages/builder-registry.ts
+++ src/pages/builder-registry.ts
@@ -27,7 +27,7 @@ const BuilderComponents = () => {
       'https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F6bef27ee40d24f3b88239fd7e616f82a',
   })
   Builder.registerComponent(TextHero, {
-    name: 'textHeroComponent',
+    name: 'Home Page Hero Banner Component',
     inputs: [
       {
         name: 'FirstImage',
