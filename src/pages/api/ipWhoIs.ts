f7faf6984def6ae302f0ea9a0117bc5164160ac8
Hanumant <hanumant.b@ignitiv.com>
2024-10-24 12:51:27 +0530

    Modify some code

--- src/pages/api/ipWhoIs.ts
+++ src/pages/api/ipWhoIs.ts
@@ -3,28 +3,42 @@ import * as cookienext from 'cookies-next'
 import GetThemeSettings from './getThemeSettings'
 
 const IpWhoIs = async () => {
-  const themeSettingsValues = await GetThemeSettings()
-  const countryCode = themeSettingsValues?.data?.ipBasedCountryCode
-  if (!cookienext.getCookie('ipBasedCountryCode')) {
-    const expiryDate = new Date()
-    expiryDate.setMonth(expiryDate.getMonth() + 1)
-    if (countryCode === 'US' || countryCode === 'CA') {
-      cookienext.setCookie('ipBasedCountryCode', countryCode, {
-        expires: expiryDate,
-        secure: true,
-      })
-    } else {
-      const response = await fetch('https://api.ipify.org?format=json')
-      const data = await response.json()
+  try {
+    const themeSettingsValues = await GetThemeSettings()
 
-      const res = await fetch(`https://ipwhois.app/json/${data.ip}?key=TvQbfSbE46pxtkHP`)
-      const ipData = await res.json()
-
-      cookienext.setCookie('ipBasedCountryCode', ipData.country_code, {
-        expires: expiryDate,
-        secure: true,
-      })
+    if (!themeSettingsValues || !themeSettingsValues?.data) {
+      console.error('Failed to load theme settings')
+      return
     }
+
+    const countryCode = themeSettingsValues.data.ipBasedCountryCode
+
+    if (!cookienext.getCookie('ipBasedCountryCode')) {
+      const expiryDate = new Date()
+      expiryDate.setMonth(expiryDate.getMonth() + 1)
+
+      // Make sure countryCode is not undefined or null
+      if (countryCode && (countryCode === 'US' || countryCode === 'CA')) {
+        cookienext.setCookie('ipBasedCountryCode', countryCode, {
+          expires: expiryDate,
+          secure: true,
+        })
+      } else {
+        // Fallback to IP-based country detection
+        const response = await fetch('https://api.ipify.org?format=json')
+        const data = await response.json()
+
+        const res = await fetch(`https://ipwhois.app/json/${data.ip}?key=TvQbfSbE46pxtkHP`)
+        const ipData = await res.json()
+
+        cookienext.setCookie('ipBasedCountryCode', ipData.country_code, {
+          expires: expiryDate,
+          secure: true,
+        })
+      }
+    }
+  } catch (error) {
+    console.error('Error fetching IP-based country code:', error)
   }
 }
 
