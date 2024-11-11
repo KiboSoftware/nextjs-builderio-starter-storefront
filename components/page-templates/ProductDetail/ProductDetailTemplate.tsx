832b4b60002d244ae45d6a83c77d61ed41c5151e
Prathmesh Panchwagh <Prathmesh.p@ignitiv.com>
2024-11-11 13:36:07 +0530

    WEB-978 Typography added for text.

--- components/page-templates/ProductDetail/ProductDetailTemplate.tsx
+++ components/page-templates/ProductDetail/ProductDetailTemplate.tsx
@@ -407,7 +407,7 @@ const ProductDetailTemplate = (props: ProductDetailTemplateProps) => {
         />
         <Box paddingY={1} display={shortDescription ? 'block' : 'none'}>
           <Box
-            sx={{ color: '#000', fontSize: '16px', fontWeight: 300, lineHeight: '25px' }}
+            sx={{ fontSize: (theme) => theme.typography.body2, color: '#000' }}
             data-testid="short-description"
             dangerouslySetInnerHTML={{
               __html: shortDescription,
