abd16b7208db5049d7d9300991326e74ef79e755
Prathmesh Panchwagh <Prathmesh.p@ignitiv.com>
2024-11-16 09:52:45 +0530

    WEB- 1092 & WEB- 1093 Bugs Resolved

--- components/product/ProductCardListView/ProductCardListView.styles.ts
+++ components/product/ProductCardListView/ProductCardListView.styles.ts
@@ -35,7 +35,6 @@ export const ProductCardStyles = {
     },
     position: 'relative',
     zIndex: 1,
-    mt: 3,
   },
   shopNow: { width: '100%', marginTop: '3.063rem' },
   hoveredButtons: {
@@ -91,6 +90,7 @@ export const ProductCardStyles = {
     padding: '0px 10px',
     alignItems: 'end',
     marginBottom: '30px',
+    marginTop: '30px',
   },
   brandLable: {
     color: '#020027',
