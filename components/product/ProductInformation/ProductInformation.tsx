80104cc2604b614b87c437b74025333f5931208e
Prathmesh Panchwagh <Prathmesh.p@ignitiv.com>
2024-11-19 14:49:35 +0530

    WEB-982 HTML mark-up issue Resolved.

--- components/product/ProductInformation/ProductInformation.tsx
+++ components/product/ProductInformation/ProductInformation.tsx
@@ -37,7 +37,7 @@ const StyledAccordion = styled(Accordion)(({ theme }) => ({
 }))
 
 interface ProductInformationProps {
-  productFullDescription: string | undefined
+  productFullDescription: any
   options: CrProductOption[]
 }
 
@@ -56,7 +56,7 @@ const ProductInformation = (props: ProductInformationProps) => {
       <Box
         sx={{ fontSize: (theme) => theme.typography.body2, color: 'grey.900' }}
         dangerouslySetInnerHTML={{
-          __html: productFullDescription as string,
+          __html: productFullDescription,
         }}
         data-testid="product-content"
       />
