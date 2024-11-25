bb4d726811b90f69eb4d4ebe034cbea846d3c6da
Hanumant Bangar <hanumant.b@ignitiv.com>
2024-11-25 17:46:57 +0530

    Fixed Bug WEB-1082 and WEB-1084

--- components/product/ProductCardListView/ResourceProductCardListView.tsx
+++ components/product/ProductCardListView/ResourceProductCardListView.tsx
@@ -49,6 +49,16 @@ const brandImages: Record<string, string> = {
   fortis: fortis.src,
 }
 
+const styles = {
+  shortDesc: {
+    fontSize: '14px',
+    fontWeight: 300,
+    fontFamily: 'poppins',
+    lineHeight: 'normal',
+    color: '#333',
+  },
+}
+
 const ProductCardSkeleton = () => {
   return (
     <Stack spacing={1} sx={ProductCardStyles.cardRoot} data-testid="product-card-skeleton">
@@ -182,10 +192,11 @@ const ResourceProductCardListView = (props: ProductCardListViewProps) => {
               <Box flexDirection="column" m={1} width="75%" className="product-info">
                 <Box display="flex" alignItems="center" width="100%">
                   <Typography
-                    variant="body1"
+                    variant="body2"
                     gutterBottom
                     color="text.primary"
-                    sx={ProductCardStyles.productTitle}
+                    fontWeight="500"
+                    sx={{ ...ProductCardStyles.productTitle }}
                   >
                     {title}
                   </Typography>
@@ -260,6 +271,7 @@ const ResourceProductCardListView = (props: ProductCardListViewProps) => {
                 /> */}
                 <Box>
                   <Box
+                    sx={styles.shortDesc}
                     data-testid="short-description"
                     dangerouslySetInnerHTML={{
                       __html: productDescription,
