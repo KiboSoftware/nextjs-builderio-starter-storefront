4db62770729e60994429a0df448cea16a6bfed67
Hanumant Bangar <hanumant.b@ignitiv.com>
2024-12-02 13:47:55 +0530

    Code changes done for the WEB-1063-checkout-footer

--- components/layout/Footer/Footer.tsx
+++ components/layout/Footer/Footer.tsx
@@ -1,9 +1,14 @@
 import { useEffect, useState } from 'react'
 
 import { BuilderComponent, builder } from '@builder.io/react'
+import { useRouter } from 'next/router'
+
+import CheckoutFooter from '@/components/checkout/CheckoutFooter/CheckoutFooter'
 
 const FooterSection = () => {
   const [footerContent, setFooterContent] = useState(null)
+  const [isCheckout, setIsCheckout] = useState(false)
+  const router = useRouter()
 
   useEffect(() => {
     async function fetchFooterContent() {
@@ -12,12 +17,17 @@ const FooterSection = () => {
     }
 
     fetchFooterContent()
+    setIsCheckout(router.pathname === '/checkout/[checkoutId]' ? true : false)
   }, [])
 
   return (
     <footer>
       {footerContent ? (
-        <BuilderComponent model="footer" content={footerContent} />
+        isCheckout ? (
+          <CheckoutFooter />
+        ) : (
+          <BuilderComponent model="footer" content={footerContent} />
+        )
       ) : (
         <p>Loading footer...</p>
       )}
